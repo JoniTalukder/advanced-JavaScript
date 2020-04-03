52b6981aa12054041482136daf0679bcebc44fe1
JoniTalukder <jonictg7@gmail.com>
2020-04-03 21:23:57 +0600

    duble vs thiple equals

---
+++ double-vs-thriple-equals.js
@@ -0,0 +1,17 @@
+const first = 2;
+const second = "2";
+if(first == second){
+    console.log("Condition is True");
+}else{
+    console.log("Condition is False");
+}
+
+//Thiple Equals
+
+const num1 = 2;
+const num2 = "2";
+if(num1 === num2){
+    console.log("Condition is True");
+}else{
+    console.log("Condition is False");
+}
