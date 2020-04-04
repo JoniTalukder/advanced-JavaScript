c070dd4bf7b973cc3518172d489872ccccde1c2d
JoniTalukder <jonictg7@gmail.com>
2020-04-04 22:00:07 +0600

    property

---
+++ object-property.js
@@ -0,0 +1,8 @@
+const students = [
+    {id: 1, Name:'Joni'},
+    {id: 2, Name:'Momi'},
+    {id: 3, Name:'Roni'},
+    {id: 4, Name:'Dipro'},
+];
+const result = students.map(s => s.id);
+console.log(result);
